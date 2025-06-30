e787624671ad08faf55be4e476f40ea459979dc9
Erick <erickmachado050@gmail.com>
2025-06-30 14:21:30 -0300

    sequencia de escape

--- ex004/index.php
+++ ex004/index.php
@@ -10,8 +10,19 @@
         $nome = 'Erick';
         $sobrenome = "Machado";
         const ESTADO = 'RS';   
-        echo "Olá! eu me chamo $nome $sobrenome e moro no " .ESTADO;
+        echo "Olá! eu me chamo $nome $sobrenome e moro no " .ESTADO . "<br>";
+
+        #aprendendo a sequencia de escape: 
+        # \n -> nova linha
+        # \t -> Tabulação horizontal
+        # \\ -> exibir barra invertida "\"
+        # \$ -> mostrar sifrão "$"
+        # \u{} -> Codepoint Unicode
+        # \"\" -> coloca aspas duplas
+
+        $nom = "Rodrigo";
+        $sobrenom = "Nogueira";
+        echo "E meu lutador favorito é o $nom \"Minotauro\" $sobrenom"
     ?>
-    </h1>
 </body>
 </html>
